7c8ea01a9659852a0d923fbff0f88795b459934e
afabor <94461187+afabor@users.noreply.github.com>
2024-02-09 10:39:19 +0000

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,15 @@
+movie_list = ['HSM', 'Rush Hour', 'Avengers', 'Spoderman']
+
+print(movie_list[0])
+print(movie_list[3])
+
+movie_list[2]= 'Walking Dead'
+print(movie_list)
+## APPEND ADDS TO THE LIST
+movie_list.append('DBZ Ressurection of F')
+print(movie_list)
+## POP TAKES THE LAST ITEM FROM THE LIST (DOES NOT NEED ANY PARAMETERS IN BRACKETS)
+movie_list.pop()
+print(movie_list)
+movie_list.sort()
+
